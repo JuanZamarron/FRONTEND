0cc451e9dea414ea46aef2f398a751d0eb4ff0dc
JuanZamarron <47125895+JuanZamarron@users.noreply.github.com>
2019-11-25 18:23:10 -0600

    Update signup.js

--- js/signup.js
+++ js/signup.js
@@ -14,7 +14,7 @@ $('#signup_button').on('click', function(){
   json_to_send = JSON.stringify(json_to_send);
 
   $.ajax({
-    url: 'https://backend-a00815058.herokuapp.com/users',
+    url: 'http://localhost:3000/users',
     // url: 'https://tuapp.herokuapp.com/users',
     headers: {
         'Content-Type':'application/json'
