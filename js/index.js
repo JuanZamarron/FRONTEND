c450a28dd3ba2731416b236a7a71ce4881630c39
JuanZamarron <47125895+JuanZamarron@users.noreply.github.com>
2019-11-25 18:22:51 -0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -11,7 +11,7 @@ $('#login_button').on('click', function(){
   json_to_send = JSON.stringify(json_to_send)
   console.log(json_to_send)
   $.ajax({
-    url: 'https://backend-a00815058.herokuapp.com/login',
+    url: 'http://localhost:3000/login',
     // url: 'https://tuapp.herokuapp.com/users/login',
     headers: {
         'Content-Type':'application/json'
